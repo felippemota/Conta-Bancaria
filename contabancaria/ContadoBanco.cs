99cfd6a622ece3edd83096d28acc3a7f064cf964
FELIPPE SANTOS MOTA MOTA <felippemota@outlook.com.br>
2022-04-07 13:26:30 -0300

    Adicionar arquivos de projeto.

---
+++ contabancaria/ContadoBanco.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.IO;
+using System.Globalization;
+namespace SistemadeBanco
+{
+    public class ContaBancaria
+    {
+
+        public string Nome { get; set; }
+        public int Numero { get; private set; }
+        public float Saldo { get; private set; }
+
+
+        public ContaBancaria(string nome, int numero)
+        {
+            Nome = nome;
+            Numero = numero;
+
+        }
+        public ContaBancaria(int numero, string nome, float depositoinicial)
+        {
+            Nome = nome;
+            Numero = numero;
+            Saldo = depositoinicial;
+        }
+
+
+        public void Deposito(double quantia)
+
+        {
+            Saldo += (float)quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= (float)quantia + 5;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + Numero
+                + ", Titular: "
+                + Nome
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+
+}
---
+++ contabancaria/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SistemadeBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n)");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.Write("Digite o depósito inicial: ");
+                float depositoinicial = float.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, titular, depositoinicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(titular, numero);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o valor a ser depositado: ");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(conta);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o valor a ser sacado: ");
+            double saque = double.Parse(Console.ReadLine());
+            conta.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados");
+            Console.WriteLine(conta);
+
+        }
+
+    }
+}
+
